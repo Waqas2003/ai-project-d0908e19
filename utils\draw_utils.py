8194be6a2f19603e12152aa58d73472bf70155b0
Waqas Bin Niaz <140584666+Waqas2003@users.noreply.github.com>
2025-06-30 20:44:44 +0500

    Add utils\draw_utils.py

---
+++ utils\draw_utils.py
@@ -0,0 +1,10 @@
+import pygame
+
+def draw_text(text, font, color, surface, x, y):
+    text_obj = font.render(text, 1, color)
+    text_rect = text_obj.get_rect()
+    text_rect.topleft = (x, y)
+    surface.blit(text_obj, text_rect)
+```
+
+**main.py:**
